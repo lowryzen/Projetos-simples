93bd33e992e8407c8d436afa889cd986e2426bdc
lowryzen <lowhack@tutamail.com>
2024-11-09 13:33:46 -0300

    Add files via upload

---
+++ #Calculadora beta 0.py
@@ -0,0 +1,22 @@
+#Calculadora beta 0.1
+
+pergunta = input("Conta: ",)
+
+
+x = float()
+y = float()
+
+def mais():
+  mais = (x + y)
+
+def menos():
+ return (x - y)
+
+def vezes():
+ return (x * y)
+
+def dividido_por():
+ return  (x / y)
+
+if pergunta == int (f"({x} + {y}"):
+    print (x + y)
