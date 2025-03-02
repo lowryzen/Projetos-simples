f45f5b0f84b793295e6438209cb0ab0574868e72
lowryzen <lowhack@tutamail.com>
2025-03-02 20:53:54 -0300

    Adicionei novos recursos

--- Automação Web.py
+++ Automação Web.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 class Web_auto:
     def __init__(self, url:str):
@@ -8,16 +9,52 @@ class Web_auto:
     def __str__(self):
         return self.navegador.title
 
-    def clicar(self, local, nome, segundos_espera=0):
+    def clicar(self, local, nome:str, segundos_espera:int =0):
         """Informe o local do código html e o nome que deseja clicar, e quantos segundos deseja esperar até clicar"""
 
         self.navegador.implicitly_wait(segundos_espera)
         clique = self.navegador.find_element(local, nome)
         clique.click()
 
-    def pegar_info(self, local, nome, segundos_espera=0):
+    def pegar_info(self, local, nome:str, segundos_espera:int =0):
         """Pega as informações com base no local do html e o nome, e quantos segundos deseja esperar até carregar"""
 
         self.navegador.implicitly_wait(segundos_espera)
         info = self.navegador.find_element(local, nome)
-        print(info.text)
+        return info
+
+    def pegar_infos(self, local, nome:str, segundos_espera:int = 0):
+        """Pega várias informações com base no local do html e o nome, e quantos segundos deseja esperar"""
+        self.navegador.implicitly_wait(segundos_espera)
+        infos = self.navegador.find_elements(local, nome)
+        return infos
+
+    def voltar(self):
+        """Volta uma aba do navegador"""
+
+        self.navegador.back()
+
+    def esperar_ate(self, segundos):
+        """Espera alguns segundos até acontecer algo"""
+        WebDriverWait(self.navegador, segundos)
+
+    def executar_script(self, codigo, elemento):
+        """Executa um código em javascript no navegador"""
+
+        self.navegador.execute_script(codigo, elemento)
+
+    def minimizar_janela(self):
+        """Minimiza o navegador"""
+
+        self.navegador.minimize_window()
+
+    def maximizar_janela(self):
+        """Preenche a tela toda com o navegador"""
+
+        self.navegador.maximize_window()
+
+
+    def sair_navegador(self):
+        """Fecha o navegador"""
+
+        self.navegador.quit()
